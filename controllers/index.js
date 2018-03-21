820cf9046adcc484641ca772ad337195489e2ca6
Kris Wozencroft <kriswozy@Kriss-MacBook-Pro.local>
2018-03-21 10:42:57 +0000

    twit added

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,19 @@
 const PersonalityInsights = require('watson-developer-cloud/personality-insights/v3')
-const personalityConfig = require('../config')
+const {personalityConfig, twitterKeys} = require('../config')
 const username = personalityConfig.username
 const password = personalityConfig.password
+const Twit = require('twit')
 
 const pi = new PersonalityInsights({
     username,
     password,
     version_date: '2017-12-12'
+})
+
+const T = new Twit({
+    consumer_key: twitterKeys.consumer_key,
+    consumer_secret: twitterKeys.consumer_secret,
+    app_only_auth: true
 })
 
 function analysePersonality(req, res, next) {
